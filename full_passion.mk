1731db41f1dd81f6051cf5bd4d85a0c62ecb03cd
Spector <33796688+Spector0@users.noreply.github.com>
2017-12-16 10:53:50 +0530

    Update full_passion.mk

--- full_passion.mk
+++ full_passion.mk
@@ -21,9 +21,6 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from passion device
 $(call inherit-product, device/lenovo/passion/device.mk)
 
-# Device vendor
-$(call inherit-product, vendor/lenovo/passion/passion-vendor.mk)
-
 # Device identifier. This must come after all inclusions
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
 PRODUCT_DEVICE := passion
